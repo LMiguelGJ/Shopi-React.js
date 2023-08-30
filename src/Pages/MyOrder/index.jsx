53a4ac12dadabc3daf72044cb87bc09c2e13fad8
Luis Miguel <l.miguel.gonzalez.j@gmail.com>
2023-08-30 09:21:19 -0400

    23 Página de MyOrders: lista de órdenes

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -2,14 +2,22 @@ import { useContext } from 'react'
 import Layout from '../../Components/Layout'
 import OrderCard from '../../Components/OrderCard'
 import { ShoppingCardContext } from '../../Context';
+import { ChevronLeftIcon } from '@heroicons/react/24/solid';
+import { Link } from 'react-router-dom';
 
 function MyOrder() {
   const context = useContext(ShoppingCardContext)
   console.log(context.order)
   return (
     <Layout>
-      MyOrder
-      <div className='flex flex-col w-80'>
+      <div className="flex w-80 items-center justify-center relative">
+        <Link to='/My-Orders' className="absolute left-0">
+          <ChevronLeftIcon className="h-6 w-6 text-black 
+                        cursor-pointer"></ChevronLeftIcon>
+        </Link>
+        <h1>My Order</h1>
+      </div>
+      <div className='flex flex-col w-80 gap-2 mt-6'>
         {
           context.order?.slice(-1)[0].product.map(product => (
             <OrderCard
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -4,20 +4,23 @@ import OrdersCard from "../../Components/OrdersCard";
 import { ShoppingCardContext } from "../../Context";
 import { Link } from "react-router-dom";
 
+
 function MyOrders() {
     const context = useContext(ShoppingCardContext)
 
     return (
         <>
-            <Layout className='bg-red-100'>
-            MyOrders
+            <Layout>
+                <div className="flex w-80 items-center justify-center relative">
+                    <h1>My Orders</h1>
+                </div>
             {
                 context.order.map((order, index) => {
                     <Link key={index} to={`/my-orders/${order.id}`}>
                     <OrdersCard
-                     totalPrice={order.totalPrice} 
-                     totalProducts={order.totalProducts}>
-                     </OrdersCard>
+                        totalPrice={order.totalPrice} 
+                        totalProducts={order.totalProducts}>
+                    </OrdersCard>
                     </Link>
                 })
             }
